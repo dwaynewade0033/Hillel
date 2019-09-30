890fa5df7159284b57c4daa7a08f460e9b85f6ef
dwaynewade0033 <dwaynewade0033@gmail.com>
2019-09-30 17:18:52 +0300

    Generics 2

---
+++ src/HomeWorkGenerics2/Main.java
@@ -0,0 +1,44 @@
+package HomeWorkGenerics2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> finalList = new ArrayList<>();
+        List<Integer> finalwList = new ArrayList<>();
+        finalList.add(3);
+        finalList.add(4);
+        finalList.add(5);
+        finalList.add(6);
+        finalwList.add(3);
+        finalwList.add(4);
+        finalwList.add(5);
+        columns(finalList,finalwList);
+
+
+    }
+
+
+    public static <T> void finalCol(List<T> first, List<T> second){
+        Set<T> finalSet = new TreeSet<>();
+        finalSet.addAll(first);
+        finalSet.addAll(second);
+        System.out.println(finalSet);
+    }
+
+
+    public static <T> void  columns(List<T> first, List<T> second){
+         List<T> finalList = new ArrayList<T>();
+         finalList = first;
+        finalList.retainAll(second);
+        System.out.println(finalList);
+
+    }
+
+
+
+    }
