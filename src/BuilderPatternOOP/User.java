f436b4b6b132443092c52918786732f7590c091e
dwaynewade0033 <dwaynewade0033@gmail.com>
2020-04-14 15:51:40 +0300

    BuilderPatternRefix

--- src/BuilderPatternOOP/User.java
+++ src/BuilderPatternOOP/User.java
@@ -8,6 +8,7 @@ public class User {
     private String phone;
     private String password;
     private String sex;
+    private String sexx;
 
     public String getFirstName() {
         return firstName;
